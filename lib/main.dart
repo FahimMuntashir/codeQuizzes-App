2cab9691cc246a0a1d0ba72c65a9e518a54a0747
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-08 17:01:40 +0600

    add List

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,75 @@
+import 'package:codequiz/model/question.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CodeQuiz(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CodeQuiz  extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _State createState() => _State();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _State extends State<CodeQuiz> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List questionBank = [
+    Question.name('Only character or integer can be used in switch statement', false),
+    Question.name('The return type of malloc function is void.', false),
+    Question.name('define is known as preprocessor compiler directive.', true),
+    Question.name('Algorithm is the graphical representation of logic.', false),
+    Question.name('sizeof( ) is a function that returns the size of a variable.',false ),
+    Question.name('It is necessary that a loop counter must only be an int. It cannot be a float.',false ),
+    Question.name('The ++ operator increments the operand by 1, whereas, the -- operator decrements it by 1.',true ),
+    Question.name('A zero value is considered to be false and a non-zero value is considered to be true.', true),
+    Question.name('= is used for comparison, whereas, == is used for assignment of two quantities.', false),
+    Question.name('Blank spaces may be inserted between two words to improve the readability of the statement.', true),
+    Question.name('The keywords cannot be used as variable names.', true),
+    Question.name('continue keyword skip one iteration of loop?', true),
+    Question.name('Switch statement can have any number of case instances', true),
+    Question.name('Two case constants within the same switch statement can have the same value.', false),
+    Question.name('ferror( ) reports any error that might have occurred during a read/write operation on a file.', true),
+    Question.name('A file opened for writing already exists its contents would be overwritten', true),
+    Question.name('Structure is collection of dissimilar data types.', true),
+    Question.name('A do-while loop is used to ensure that the statements within the loop are executed at least twice.', false),
 
+
+  ];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Code Quiz'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Center(
+              child: Image.asset('images/11.png',
+              height: 200,
+              width: 300,),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Container(
+              height: 120.0,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(questionBank[0]),
+              ),
             ),
+            Spacer(),
           ],
+
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
